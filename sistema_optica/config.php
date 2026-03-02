bbb824fea73a2926e22efd160bbc5352ed300905
Manuel Salazar <ibared6@gmail.com>
2026-03-02 14:02:35 -0700

    Actualizacion de diseno premium en vistas

--- sistema_optica/config.php
+++
@@ -1,4 +0,0 @@
-<?php
-ini_set('session.cookie_domain', '192.168.1.96');
-session_start();
-?>
--- sistema_optica_appi - copia/routes/api.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\Api\AuthController;
-
-Route::post('/login', [AuthController::class, 'login']);
-Route::get('/cobrador/contratos-no-cobrados/{idCobrador}', [ContratoController::class, 'contratosNoCobrados']);
